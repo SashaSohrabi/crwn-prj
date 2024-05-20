785548ce74ec0936177a7c1a3aeeae8697d7253b
Sohrabi Sasha <shahab.sohrabi@mimacom.com>
2024-05-20 15:18:50 +0200

    crwn: Use Styled-Component for Button

--- src/components/sign-in-form/sign-in-form.component.jsx
+++ src/components/sign-in-form/sign-in-form.component.jsx
@@ -1,11 +1,10 @@
 import { useState } from 'react';
 import {
   signInWithGooglePopup,
-  createUserDocumentFromAuth,
   signInAuthUserWithEmailAndPassword,
 } from '../../utils/firebase/firebase.utils';
 import FormInput from '../form-input/form-input.component';
-import Button from '../button/button.component';
+import Button, {BUTTON_TYPE_CLASSES} from '../button/button.component';
 
 import './sign-in-form.styles.scss';
 const defaultFormFields = {
@@ -75,7 +74,7 @@ const SignInForm = () => {
         />
         <div className="buttons-container">
           <Button type="submit">Sign In</Button>
-          <Button type="button" buttonType="google" onClick={signInWithGoogle}>
+          <Button buttonType={BUTTON_TYPE_CLASSES.google} type="button" onClick={signInWithGoogle}>
             Sign in with Google
           </Button>
         </div>
