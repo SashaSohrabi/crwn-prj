e1f88852dbfe94232c02c32d158d18189bcc43f3
Sohrabi Sasha <shahab.sohrabi@mimacom.com>
2024-05-21 17:45:33 +0200

    crwn: Create and use react-redux selector for user

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,16 +1,21 @@
 import { useContext } from 'react';
-import { UserContext } from '../../contexts/user.context';
 import { CartContext } from '../../contexts/cart.context';
 import { Outlet } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 import { signOutUser } from '../../utils/firebase/firebase.utils';
 import CartIcon from '../../components/cart-icon/cart-icon.component';
 import CartDropdown from '../../components/cart-dropdown/cart-dropdown.component';
-
+import { selectCurrentUser } from '../../store/user/user.selector';
 import { ReactComponent as CrownLogo } from '../../assets/crown.svg';
-import { NavigationContainer, NavLinks, NavLink, LogoContainer } from './navigation.styles';
+import {
+  NavigationContainer,
+  NavLinks,
+  NavLink,
+  LogoContainer,
+} from './navigation.styles';
 
 const Navigation = () => {
-  const { currentUser } = useContext(UserContext);
+  const currentUser = useSelector(selectCurrentUser);
   const { isCartOpen } = useContext(CartContext);
   return (
     <>
@@ -19,17 +24,13 @@ const Navigation = () => {
           <CrownLogo className="logo" />
         </LogoContainer>
         <NavLinks>
-          <NavLink to="/shop">
-            SHOP
-          </NavLink>
+          <NavLink to="/shop">SHOP</NavLink>
           {currentUser ? (
-            <NavLink as='span' onClick={signOutUser}>
+            <NavLink as="span" onClick={signOutUser}>
               SIGN OUT
             </NavLink>
           ) : (
-            <NavLink to="/auth">
-              SIGN IN
-            </NavLink>
+            <NavLink to="/auth">SIGN IN</NavLink>
           )}
           <CartIcon />
         </NavLinks>
