01e2d32d3579dabac10ff478a476a77606e80d69
Sohrabi Sasha <shahab.sohrabi@mimacom.com>
2024-06-10 14:14:37 +0200

    crwn: Typing Redux Category Files

--- src/store/categories/category.reducer.ts
+++ src/store/categories/category.reducer.ts
@@ -1,12 +1,18 @@
 import { CATEGORIES_ACTION_TYPES } from './category.types';
+import { CategoryAction } from './category.action';
+import { Category } from './category.types';
 
-export const CATEGORIES_INITIAL_STATE = {
+export type CategoriesState = {
+  readonly categories: Category[]
+}
+
+export const CATEGORIES_INITIAL_STATE: CategoriesState = {
   categories: [],
 };
 
 export const categoriesReducer = (
   state = CATEGORIES_INITIAL_STATE,
-  action = {}
+  action = {} as CategoryAction
 ) => {
   const { type, payload } = action;
 
--- src/store/categories/category.selector.ts
+++ src/store/categories/category.selector.ts
@@ -1,6 +1,8 @@
 import { createSelector } from 'reselect';
+import {  Category, CategoryMap } from './category.types';
+import { CategoriesState } from './category.reducer';
 
-const selectCategoryReducer = (state) => state.categories;
+const selectCategoryReducer = (state: any): CategoriesState => state.categories;
 
 export const selectCategories = createSelector(
   [selectCategoryReducer],
@@ -10,9 +12,9 @@ export const selectCategories = createSelector(
 export const selectCategoriesMap = createSelector(
   [selectCategories],
   (categories) =>
-    categories.reduce((acc, category) => {
+    categories.reduce((acc: CategoryMap, category: Category) => {
       const { title, items } = category;
       acc[title.toLowerCase()] = items;
       return acc;
-    }, {})
+    }, {} as CategoryMap)
 );
