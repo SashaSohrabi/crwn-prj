439b2d87bd51db9eacc91c03824c82cb243f28a5
Sohrabi Sasha <shahab.sohrabi@mimacom.com>
2024-06-10 15:57:14 +0200

    crwn: Typing Redux Cart Files

---
+++ src/store/cart/cart.reducer.ts
@@ -0,0 +1,30 @@
+import { AnyAction } from 'redux';
+import { CART_ACTION_TYPES, CartItem } from './cart.types';
+
+export type CartState = {
+  isCartOpen: boolean;
+  cartItems: CartItem[];
+};
+
+const CART_INITIAL_STATE: CartState = {
+  isCartOpen: false,
+  cartItems: [],
+};
+
+export const cartReducer = (state = CART_INITIAL_STATE, action: AnyAction): CartState => {
+
+  switch (action.type) {
+    case CART_ACTION_TYPES.SET_CART_ITEMS:
+      return {
+        ...state,
+        cartItems: action.payload,
+      };
+    case CART_ACTION_TYPES.SET_IS_CART_OPEN:
+      return {
+        ...state,
+        isCartOpen: action.payload,
+      };
+    default:
+      return state;
+  }
+};
